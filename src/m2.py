ef054da79aa75a2613e87156f0c412cfe3f46219
shih <shih@rose-hulman.edu>
2018-10-25 10:25:31 -0400

    Implemented go straight inches and touch sensor

--- src/m2.py
+++ src/m2.py
@@ -9,6 +9,8 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
-
-
+    run_test_go_straight_inches()
+def run_test_go_straight_inches():
+    robot = rb.Snatch3rRobot()
+    robot.drive_system.go_straight_inches(10)
 main()
