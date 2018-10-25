058eb89c42804f90d64440b5732e31ace6869e83
Loco9 <codylj@rose-hulman.edu>
2018-10-25 10:21:23 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/m2.py
    #	src/rosebotics.py

--- src/m2.py
+++ src/m2.py
@@ -9,11 +9,6 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
-    run_tests()
-
-def run_tests():
-    robot = rb.Snatch3rRobot()
-    robot.drive_system.go_straight_inches(10)
 
 
 main()
