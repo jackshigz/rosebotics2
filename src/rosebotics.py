fc22e440f97aeb2a35ae7da20dd7697d8d0c7339
shih <shih@rose-hulman.edu>
2018-10-25 10:56:25 -0400

    Implemented go straight inches and touch sensor

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -105,11 +105,11 @@ class DriveSystem(object):
         Go straight at the given speed (-100 to 100, negative is backwards)
         for the given number of inches, stopping with the given StopAction.
         """
-        c = 100
+        c = 87
+        self.left_wheel.reset_degrees_spun()
         self.start_moving(duty_cycle_percent,duty_cycle_percent)
-        start_spun = self.left_wheel.get_degrees_spun()
         while True:
-            if self.left_wheel.get_degrees_spun() - start_spun > inches*c:
+            if self.left_wheel.get_degrees_spun() > inches*c:
                 self.stop_moving(stop_action)
         # DONE: Do a few experiments to determine the constant that converts
         # DONE:   from wheel-degrees-spun to robot-inches-moved.
