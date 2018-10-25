d6216741691300e2ef9768c3283dc5c82d299d5e
shih <shih@rose-hulman.edu>
2018-10-25 11:17:59 -0400

    Implemented turn degrees

--- src/m1.py
+++ src/m1.py
@@ -9,10 +9,15 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
-    run_test_go_straight_inches()
+    # run_test_go_straight_inches()
+    run_test_turn_degrees()
 
 def run_test_go_straight_inches():
     robot = rb.Snatch3rRobot()
     robot.drive_system.go_straight_inches(20)
+
+def run_test_turn_degrees():
+    robot = rb.Snatch3rRobot()
+    robot.drive_system.turn_degrees(90)
 
 main()
