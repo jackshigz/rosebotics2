5e5dd673270688a78fc8b38a97e4d68616dbce90
Loco9 <codylj@rose-hulman.edu>
2018-10-28 15:05:08 -0400

    color sensor test

--- src/Logan.py
+++ src/Logan.py
@@ -10,29 +10,33 @@ import time
 def main():
     """ Runs YOUR specific part of the project """
 
-    # black_line()
-    see_color()
+    black_line()
+    # run_test_see_color()
 
 
 def black_line():
 
     robot = rb.Snatch3rRobot()
 
+    robot.drive_system.move_for_seconds(10)
     while True:
-        robot.drive_system.move_for_seconds(30)
+
         if robot.color_sensor.get_value() < 50:
             robot.drive_system.turn_degrees(1)
 
 
-def see_color():
-    robot = rb.Snatch3rRobot()
+def run_test_see_color():
 
-    color = 'red'
+    see_color('red')
+
+
+def see_color(color):
+    robot = rb.Snatch3rRobot()
 
     while True:
         robot.drive_system.go_straight_inches(10, 80)
         if robot.color_sensor.get_color() == color:
             robot.drive_system.stop_moving()
 
-main()
 
+main()
