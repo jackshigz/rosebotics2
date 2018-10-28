4b4ddbcd6387f2878da3cff144ae04a64f96020c
shih <shih@rose-hulman.edu>
2018-10-28 14:27:21 -0400

    Implemented run polygon

--- src/Haoran.py
+++ src/Haoran.py
@@ -10,8 +10,8 @@ import time
 def main():
     """ Runs YOUR specific part of the project """
     # run_test_go_straight_inches()
-    # run_test_turn_degrees()
-    run_test_polygon()
+    run_test_turn_degrees()
+    # run_test_polygon()
 
 def run_test_go_straight_inches():
     robot = rb.Snatch3rRobot()
@@ -25,10 +25,18 @@ def run_test_polygon():
     run_polygon(6)
 
 def run_polygon(n):
-    robot = rb.Snatch3rRobot()
+    #robot = rb.Snatch3rRobot()
     degree = 360/n
     for _ in range(n):
-        robot.drive_system.go_straight_inches(4)
+        run_polygon_straight()
+        run_polygon_turn(degree)
+
+def run_polygon_straight():
+    robot = rb.Snatch3rRobot()
+    robot.drive_system.go_straight_inches(10)
+
+def run_polygon_turn(degree):
+        robot = rb.Snatch3rRobot()
         robot.drive_system.turn_degrees(degree)
 
 
