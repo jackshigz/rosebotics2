02d433bd9f4b0452ccbebf7976ac6bf117530057
shih <shih@rose-hulman.edu>
2018-11-05 11:05:49 -0500

    Merge branch 'master' of https://github.com/csse120/rosebotics2
    # Conflicts:
    #	src/rosebotics_new.py

--- src/Haoran.py
+++ src/Haoran.py
@@ -42,12 +42,11 @@ def run_polygon_turn(degree):
 
 def run_test_beep_when_see():
     robot = rb.Snatch3rRobot()
-    ev3.Sound.beep().wait()
-#    while True:
-#
-#        if robot.Blob.get_area() >= 600:
-#            ev3.Sound.beep().wait()
-#            break
+    while True:
+
+        if robot.Blob.get_area() >= 600:
+            ev3.Sound.beep().wait()
+            break
 
 
 
