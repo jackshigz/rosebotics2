faf38869a33ded9454d31b8ab57129b9a84bf5f5
Loco9 <codylj@rose-hulman.edu>
2018-11-07 23:22:12 -0500

    capstone1 and follow_black_line

--- src/Logan.py
+++ src/Logan.py
@@ -10,21 +10,26 @@ import time
 def main():
     """ Runs YOUR specific part of the project """
 
-    # black_line()
+    black_line()
     # run_test_see_color()
-    raise_arm()
+    # raise_arm()
 
 
 def black_line():
 
     robot = rb.Snatch3rRobot()
 
+    initial_time = time.time()
+
     while True:
 
         if robot.color_sensor.get_reflected_intensity() < 40:
             robot.drive_system.start_moving(40, 40)
         if robot.color_sensor.get_reflected_intensity() >= 40:
-            robot.drive_system.start_moving(60, 40)
+            robot.drive_system.start_moving(75, 10)
+        if time.time() - initial_time >= 15:
+            robot.drive_system.stop_moving()
+            break
 
 
 def run_test_see_color():
