6a60ee63012c45954145a947777021b0bbd1fb1c
Loco9 <codylj@rose-hulman.edu>
2018-11-10 12:59:15 -0500

    capstone1 and follow_black_line

--- src/capstone_1_runs_on_laptop.py
+++ src/capstone_1_runs_on_laptop.py
@@ -32,7 +32,7 @@ Authors:  David Mutchler, his colleagues, and Logan Cody.
 
 # ------------------------------------------------------------------------------
 # TODO: 3. One team member: change the following in mqtt_remote_method_calls.py:
-#                LEGO_NUMBER = 99
+#                LEGO_NUMBER = 12
 # TODO:    to use YOUR robot's number instead of 99.
 # TODO:    Commit and push the change, then other team members Update Project.
 # TODO:    Then delete this TODO.
@@ -88,18 +88,18 @@ def handle_go_forward(entry, mqtt):
     Tells the robot to go forward at the speed specified in the given entry box.
     """
     # --------------------------------------------------------------------------
-    # TODO: 6. This function needs the entry box in which the user enters
-    # TODO:    the speed at which the robot should move.  Make the 2 changes
-    # TODO:    necessary for the entry_box constructed in  setup_gui
-    # TODO:    to make its way to this function.  When done, delete this TODO.
+    # DONE: 6. This function needs the entry box in which the user enters
+    # DONE:    the speed at which the robot should move.  Make the 2 changes
+    # DONE:    necessary for the entry_box constructed in  setup_gui
+    # DONE:    to make its way to this function.  When done, delete this DONE.
     # --------------------------------------------------------------------------
     speed_string = entry.get()
     mqtt.send_message('go_forward', [speed_string])
     # --------------------------------------------------------------------------
-    # TODO: 7. For this function to tell the robot what to do, it needs
-    # TODO:    the MQTT client constructed in main.  Make the 4 changes
-    # TODO:    necessary for that object to make its way to this function.
-    # TODO:    When done, delete this TODO.
+    # DONE: 7. For this function to tell the robot what to do, it needs
+    # DONE:    the MQTT client constructed in main.  Make the 4 changes
+    # DONE:    necessary for that object to make its way to this function.
+    # DONE:    When done, delete this DONE.
     # --------------------------------------------------------------------------
 
     # --------------------------------------------------------------------------
